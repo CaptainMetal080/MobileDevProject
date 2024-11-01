e45e1d03030807d49d009f5933db52a6c38af264
susan2424 <susan.ahmadi24@ontariotechu.net>
2024-10-31 23:03:31 -0400

    gps tracking draft

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.core:core-ktx:1.9.0")
+
 }
